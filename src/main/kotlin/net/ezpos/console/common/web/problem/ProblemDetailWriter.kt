8a9771c952c40d793d6c05522480cdf6500c31b1
wongyichen <wongyichen@163.com>
2026-03-08 22:26:23 +0800

    chore(deps): upgrade Jackson namespace and clean up redundant config
    Migrate ObjectMapper import to tools.jackson namespace, remove unused
    ProblemDetailJacksonConfig, add netty DNS resolver for macOS ARM, and
    drop deprecated Hibernate dialect property.

--- src/main/kotlin/net/ezpos/console/common/web/problem/ProblemDetailWriter.kt
+++ src/main/kotlin/net/ezpos/console/common/web/problem/ProblemDetailWriter.kt
@@ -1,6 +1,6 @@
 package net.ezpos.console.common.web.problem
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import tools.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.MediaType
 import org.springframework.http.ProblemDetail
